791112602abb9f55f96df82cc0aa72421289f360
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2023-03-17 16:41:31 +0530

    feat: initialized state for dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Dice from './components/Dice';
 function App() {
+  function allNewDice() {
+    let newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+  const [dice, setDice] = useState(allNewDice());
+  const diceElements = dice.map((num) => <Dice value={num} key={num} />);
+
   return (
     <div className="app">
       <main>
-        <div className="dice-container">
-          <Dice value="1" />
-          <Dice value="2" />
-          <Dice value="3" />
-          <Dice value="4" />
-          <Dice value="5" />
-          <Dice value="6" />
-          <Dice value="1" />
-          <Dice value="2" />
-          <Dice value="3" />
-          <Dice value="4" />
-        </div>
+        <div className="dice-container">{diceElements}</div>
       </main>
     </div>
   );
