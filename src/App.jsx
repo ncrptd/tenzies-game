d6a271ebb89277acb24d41d8438d4478a6f44b8b
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2023-03-17 16:26:11 +0530

    style: initial styling for our app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
-
+import Dice from './components/Dice';
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <main>
-        <h3>hello world</h3>
+        <div className="dice-container">
+          <Dice value="1" />
+          <Dice value="2" />
+          <Dice value="3" />
+          <Dice value="4" />
+          <Dice value="5" />
+          <Dice value="6" />
+          <Dice value="1" />
+          <Dice value="2" />
+          <Dice value="3" />
+          <Dice value="4" />
+        </div>
       </main>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Dice.jsx
@@ -0,0 +1,7 @@
+export default function (props) {
+  return (
+    <div className="dice">
+      <h2>{props.value}</h2>
+    </div>
+  );
+}
