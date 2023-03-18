d1a867f10d55fa84bf50ea332c015aaf422cd95f
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2023-03-18 19:16:39 +0530

    feat: completed the main logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,80 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Dice from './components/Dice';
 import { nanoid } from 'nanoid';
+// import useWindowSize from 'react-use/lib/useWindowSize';
+import Confetti from 'react-confetti';
+
 function App() {
   const [dice, setDice] = useState(allNewDice());
 
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((dice) => dice.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((dice) => dice.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    }
+  }, [dice]);
+
+  function generateNewDice() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
   function allNewDice() {
     let newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: true,
-        id: nanoid(),
-      });
+      newDice.push(generateNewDice());
     }
     return newDice;
   }
 
   const diceElements = dice.map(({ value, isHeld, id }) => (
-    <Dice value={value} key={id} isHeld={isHeld} />
+    <Dice
+      value={value}
+      key={id}
+      isHeld={isHeld}
+      holdDice={() => holdDice(id)}
+    />
   ));
 
   function rollDice() {
-    setDice(allNewDice());
+    if (tenzies) {
+      setDice(allNewDice());
+      setTenzies(false);
+    } else {
+      setDice((prevDice) =>
+        prevDice.map((dice) => {
+          if (dice.isHeld) {
+            return { ...dice };
+          } else {
+            return generateNewDice();
+          }
+        })
+      );
+    }
+  }
+
+  function holdDice(id) {
+    setDice((prevDice) => {
+      return prevDice.map((dice) => {
+        if (dice.id === id) {
+          return { ...dice, isHeld: !dice.isHeld };
+        } else {
+          return { ...dice };
+        }
+      });
+    });
   }
 
   return (
     <div className="app">
+      {tenzies && <Confetti />}
       <main>
         <h1>Tenzies</h1>
         <p>
@@ -34,8 +82,8 @@ function App() {
           current value between rolls.
         </p>
         <div className="dice-container">{diceElements}</div>
-        <button onClick={rollDice} className="roll">
-          Roll
+        <button className="roll" onClick={rollDice}>
+          {tenzies ? 'Reset Game' : 'Roll'}
         </button>
       </main>
     </div>
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,6 +1,9 @@
 export default function Dice(props) {
   return (
-    <div className={props.isHeld ? 'dice isHeld' : 'dice'}>
+    <div
+      className={props.isHeld ? 'dice isHeld' : 'dice'}
+      onClick={props.holdDice}
+    >
       <h2>{props.value}</h2>
     </div>
   );
